73d24bbe050c0995b0918f7b1286368af47dd692
Filip Stańczak <filipst166@gmail.com>
2023-12-10 03:39:39 +0100

    Project Initialization

---
+++ src/main/kotlin/fitcon/FitConApplication.kt
@@ -0,0 +1,11 @@
+package fitcon
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FitConApplication
+
+fun main(args: Array<String>) {
+    runApplication<FitConApplication>(*args)
+}
